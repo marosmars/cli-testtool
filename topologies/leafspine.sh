72c23efe611e726ba2b54311983c78d701550de0
Maros Marsalek <mmarsalek@frinx.io>
2018-10-04 16:13:24 +0200

    Fix previous update

--- topologies/leafspine.sh
+++ topologies/leafspine.sh
@@ -1,8 +1,8 @@
-python ../mockdevice.py 0.0.0.0 11000 10001 ssh ../devices/leafspine/leaf1.json &
-python ../mockdevice.py 0.0.0.0 11001 10002 ssh ../devices/leafspine/leaf2.json &
-python ../mockdevice.py 0.0.0.0 11002 10003 ssh ../devices/leafspine/leaf3.json &
-python ../mockdevice.py 0.0.0.0 11003 10004 ssh ../devices/leafspine/leaf4.json &
-python ../mockdevice.py 0.0.0.0 11004 10005 ssh ../devices/leafspine/leaf5.json &
+python ../mockdevice.py 0.0.0.0 11000 11001 ssh ../devices/leafspine/leaf1.json &
+python ../mockdevice.py 0.0.0.0 11001 11002 ssh ../devices/leafspine/leaf2.json &
+python ../mockdevice.py 0.0.0.0 11002 11003 ssh ../devices/leafspine/leaf3.json &
+python ../mockdevice.py 0.0.0.0 11003 11004 ssh ../devices/leafspine/leaf4.json &
+python ../mockdevice.py 0.0.0.0 11004 11005 ssh ../devices/leafspine/leaf5.json &
 python ../mockdevice.py 0.0.0.0 12000 12001 ssh ../devices/leafspine/spine1.json &
 python ../mockdevice.py 0.0.0.0 12001 12002 ssh ../devices/leafspine/spine2.json &
 
