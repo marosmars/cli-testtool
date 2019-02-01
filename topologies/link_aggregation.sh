549fe56807a343a27e7776d57c9da219c9003a82
dkutenicsova <dkutenicsova@frinx.io>
2019-02-01 14:48:31 +0100

    Update link_aggregation.sh

--- topologies/link_aggregation.sh
+++ topologies/link_aggregation.sh
@@ -1,4 +1,4 @@
-python ../mockdevice.py 0.0.0.0 20001 20002 ssh ../devices/cisco_XR.json &
-python ../mockdevice.py 0.0.0.0 20002 20003 ssh ../devices/cisco_XR.json &
+python ../mockdevice.py 0.0.0.0 13001 13002 ssh ../devices/cisco_XR.json &
+python ../mockdevice.py 0.0.0.0 13002 13003 ssh ../devices/cisco_XR.json &
 
 wait
