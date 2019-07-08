bc0e7fa18db535fb520d21ba2397ad7ce03cb5f2
Ladislav Moravek <lmoravek@frinx.io>
2019-07-08 15:12:23 +0200

    Moved admin:admin to variables, so that it's easier to replace these values. They need to be replacable for FRINX Machine to work with a dynamically created user

--- util/generateMount.sh
+++ util/generateMount.sh
@@ -10,9 +10,14 @@ PROTOCOL=$4
 PORT_NUM=`expr $HIGH_PORT - $LOW_PORT`
 MOUNT_ID_PREFIX="cli-"
 
+# These two variables values are dynamically replaced by 'password-setup.sh'
+# script in FRINX Machine.
+USERNAME=""
+PASSWORD=""
+
 # Remove all cli nodes
 echo "Removing all cli nodes"
-curl -u admin:admin -H "Content-Type: application/json" -X DELETE http://localhost:8181/restconf/config/network-topology:network-topology/topology/cli/
+curl -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X DELETE http://localhost:8181/restconf/config/network-topology:network-topology/topology/cli/
 echo "
 
 Waiting a bit ...
@@ -21,7 +26,7 @@ Waiting a bit ...
 sleep 20s
 
 body="{
-    \"network-topology:node\" : 
+    \"network-topology:node\" :
     ["
 
 # \"cli-topology:keepalive-delay\": 250,
@@ -33,17 +38,17 @@ do
   body="$body
       {
       \"network-topology:node-id\" : \"$MOUNT_ID_PREFIX$port\",
-     
+
       \"cli-topology:host\" : \"$IP\",
       \"cli-topology:port\" : \"$port\",
       \"cli-topology:transport-type\" : \"$PROTOCOL\",
-      
+
       \"cli-topology:device-type\" : \"ios\",
       \"cli-topology:device-version\" : \"15.3\",
-      
+
       \"cli-topology:username\" : \"cisco\",
       \"cli-topology:password\" : \"cisco\",
-      
+
       \"cli-topology:journal-size\": 150,
       \"cli-topology:dry-run-journal-size\": 180,
 
@@ -63,7 +68,7 @@ body="$body
 echo "Sending mount requests"
 #echo $body
 
-curl -s -u admin:admin -d @- -H "Content-Type: application/json" -X POST http://localhost:8181/restconf/config/network-topology:network-topology/topology/cli/ <<CURL_DATA
+curl -s -u "${USERNAME}:${PASSWORD}" -d @- -H "Content-Type: application/json" -X POST http://localhost:8181/restconf/config/network-topology:network-topology/topology/cli/ <<CURL_DATA
 $body
 CURL_DATA
 
@@ -71,8 +76,9 @@ START=$(date +%s.%N)
 
 while (true);
 do
-	connected=`curl -s -u admin:admin  -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/cli/ | grep -o 'connection-status":"connected"' | wc -l`
-	reconciled=`curl -s -u admin:admin  -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/uniconfig/?depth=3 | grep -o "$MOUNT_ID_PREFIX" | wc -l`
+	connected=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/cli/ | grep -o 'connection-status":"connected"' | wc -l`
+
+	reconciled=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/uniconfig/?depth=3 | grep -o "$MOUNT_ID_PREFIX" | wc -l`
 	END=$(date +%s.%N)
 	DIFF=$(echo "$END - $START" | bc)
 	echo "Currently connected: $connected, took: $DIFF seconds"
@@ -92,6 +98,6 @@ done
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo "Time spent: $DIFF seconds"
-data_size=`curl -s -u admin:admin  -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/uniconfig/ | wc -c`
+data_size=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/uniconfig/ | wc -c`
 echo "Total data of json.bytes in uniconfig: $data_size bytes"
 
