29a5d4fe3c6bf4f5bf19b454ac9b468daafdb41b
Ladislav Moravek <lmoravek@frinx.io>
2019-07-09 12:08:21 +0200

    moved some hard coded values into vatiables

--- util/generateMount.sh
+++ util/generateMount.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 set -e
 
-# Usage ./generateMount.sh 127.0.0.1 10000 10050
+# Usage ./generateMount.sh cli_testtool_host low_port high_port protocol odl_host
+# Example ./generatMount.sh 127.0.0.1 10000 50000 ssh 127.0.0.1
 
-IP=$1
+CLI_TESTTOOL_HOST=$1
 LOW_PORT=$2
 HIGH_PORT=$3
 PROTOCOL=$4
+ODL_HOST="${5:-localhost}"
 PORT_NUM=`expr $HIGH_PORT - $LOW_PORT`
 MOUNT_ID_PREFIX="cli-"
+
+ODL_PORT="8181"
+SCHEME="http"
 
 # These two variables values are dynamically replaced by 'password-setup.sh'
 # script in FRINX Machine.
@@ -17,7 +22,7 @@ PASSWORD="admin"
 
 # Remove all cli nodes
 echo "Removing all cli nodes"
-curl -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X DELETE http://localhost:8181/restconf/config/network-topology:network-topology/topology/cli/
+curl -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X DELETE "${SCHEME}://${ODL_HOST}:${ODL_PORT}/restconf/config/network-topology:network-topology/topology/cli/"
 echo "
 
 Waiting a bit ...
@@ -39,7 +44,7 @@ do
       {
       \"network-topology:node-id\" : \"$MOUNT_ID_PREFIX$port\",
 
-      \"cli-topology:host\" : \"$IP\",
+      \"cli-topology:host\" : \"$CLI_TESTTOOL_HOST\",
       \"cli-topology:port\" : \"$port\",
       \"cli-topology:transport-type\" : \"$PROTOCOL\",
 
@@ -76,9 +81,9 @@ START=$(date +%s.%N)
 
 while (true);
 do
-	connected=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/cli/ | grep -o 'connection-status":"connected"' | wc -l`
+	connected=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET "${SCHEME}://${ODL_HOST}:${ODL_PORT}/restconf/operational/network-topology:network-topology/topology/cli/" | grep -o 'connection-status":"connected"' | wc -l`
 
-	reconciled=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/uniconfig/?depth=3 | grep -o "$MOUNT_ID_PREFIX" | wc -l`
+	reconciled=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET "${SCHEME}://${ODL_HOST}:${ODL_PORT}/restconf/operational/network-topology:network-topology/topology/uniconfig/?depth=3" | grep -o "$MOUNT_ID_PREFIX" | wc -l`
 	END=$(date +%s.%N)
 	DIFF=$(echo "$END - $START" | bc)
 	echo "Currently connected: $connected, took: $DIFF seconds"
@@ -98,6 +103,6 @@ done
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 echo "Time spent: $DIFF seconds"
-data_size=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET http://localhost:8181/restconf/operational/network-topology:network-topology/topology/uniconfig/ | wc -c`
+data_size=`curl -s -u "${USERNAME}:${PASSWORD}" -H "Content-Type: application/json" -X GET "${SCHEME}://${ODL_HOST}:${ODL_PORT}/restconf/operational/network-topology:network-topology/topology/uniconfig/" | wc -c`
 echo "Total data of json.bytes in uniconfig: $data_size bytes"
 
